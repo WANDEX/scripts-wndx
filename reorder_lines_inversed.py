4d12a3ae3cc53808748c6f2193b7ccac0c338e06
WANDEX <15724752+WANDEX@users.noreply.github.com>
2018-04-16 17:13:03 +0300

    added utf8 compatibility

--- reorder_lines_inversed.py
+++ reorder_lines_inversed.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Main module."""
+import codecs
+
 file = "file.txt"
 output = "output_file.txt"
 
@@ -10,5 +12,8 @@ if(output.isspace() or output == str("")):
     output = "new_" + str(file)
 print(str(file + " " + output))
 
-with open(file) as f_in, open(output, 'w') as f_out:
+with codecs.open(file, encoding='utf-8', errors='replace') as f_in, open(output, 'w') as f_out:
     f_out.writelines(reversed(f_in.readlines()))
+    f_out.write(u'\ufeff')
+    f_out.close()
+    print("SUCCESS")
