a8816fa5960fef9924c62d2ec7afb79f43b6b568
WANDEX <15724752+WANDEX@users.noreply.github.com>
2018-05-30 20:01:29 +0300

    added blocks method

--- reorder_lines_inversed.py
+++ reorder_lines_inversed.py
@@ -22,7 +22,7 @@ print(str(file + " " + output + " " + encoding))
 
 
 def reorder_method():
-    irmethod = input("reorder method('strict'/'...'): ")
+    irmethod = input("reorder method('strict'/'blocks'/'...'): ")
     return(irmethod)
 
 
@@ -31,12 +31,26 @@ def strict(f_in, f_out):
     print("SUCCESS STRICT REORDER COMPLETE")
 
 
+def blocks(f_in, f_out):
+    blocks_list = []
+    # all_lines = ""
+    for line in f_in:
+        blocks_list.insert(0, line)
+        # if not line.strip(): # empty line
+            # blocks_list.append(line)
+            # block_of_lines = ""
+
+    # all_lines = f_in.readlines()
+    f_out.writelines(blocks_list)
+    print("SUCCESS BLOCKS REORDER COMPLETE")
+
+
 def reorder():
     with open(file, 'r', 1, encoding, errors='replace') as f_in, open(output, 'w', 1, encoding) as f_out:
         if reorder_method() == "strict":
             strict(f_in, f_out)
         else:
-            pass
+            blocks(f_in, f_out)
 
 
 reorder()
