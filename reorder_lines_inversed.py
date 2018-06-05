26c75f96941d9bb4e05c9c26e63c1108e12f0c69
WANDEX <15724752+WANDEX@users.noreply.github.com>
2018-06-05 15:46:44 +0300

    relative path compatibility implementation and refactoring

--- reorder_lines_inversed.py
+++ reorder_lines_inversed.py
@@ -5,32 +5,33 @@ import os
 
 file = ""
 
-while (not os.path.exists(file)):
-    print("ENTER VALID RELATIVE FILE PATH OR FULL PATH WITH EXTENSION\n[" + str(file) + "]")
+while not os.path.exists(file):
+    print("ENTER VALID RELATIVE/FULL FILE PATH WITH EXTENSION:\n"
+          "[" + file + "]")
     file = input("file: ")
     if file.startswith("\\"):
         current_dir = os.getcwd()
         file = current_dir + file
         print("relative file path:\n" + file)
 
-output = input("output(if empty '  + _new'): ")
+output = input("output (if empty ' + _new'): ")
 
-if (output.isspace() or output == ""):
+if output.isspace() or output == "":
     pair = os.path.splitext(file)
     output = pair[0] + "_new" + pair[1]
 
-encoding = input("encoding(if empty 'UTF-8'): ").lower() or "utf-8"
+encoding = input("encoding (if empty 'UTF-8'): ").lower() or "utf-8"
 
 
 def idelimiter():
-    emptyline = "\n"
-    delimiter = input("delimiter(if 'empty line' - nothing): ")
-    return delimiter + emptyline
+    empty_line = "\n"
+    delimiter = input("delimiter (if empty - 'empty line'): ")
+    return delimiter + empty_line
 
 
-def reorder_method():
-    irmethod = input("reorder method('strict'/'blocks'/'...'): ").lower()
-    return(irmethod)
+def ireorder():
+    ireorder = input("reorder method('strict'/'blocks'): ").lower()
+    return(ireorder)
 
 
 def strict(f_in, f_out):
@@ -53,22 +54,24 @@ def blocks(f_in, f_out):
             line_index = 0
             blocks_list.insert(line_index, line)
         else:
-            print("SOMETHING HAPPENED AT LINE: {0}\n, STRING CONTENT: {1}".format(str(line_counter), str(line)))
+            print("SOMETHING HAPPENED AT LINE: {0}\n"
+                  "STRING CONTENT: {1}".format(str(line_counter), str(line)))
 
     f_out.writelines(blocks_list)
     print("SUCCESS BLOCKS REORDER COMPLETE")
 
 
-def reorder():
-    with open(file, 'r', 1, encoding, errors='replace') as f_in, open(output, 'w', 1, encoding) as f_out:
-        rmethod = reorder_method()
-        if rmethod == "strict":
+def execute():
+    with open(file, 'r', 1, encoding, errors='replace') as f_in, \
+         open(output, 'w', 1, encoding, errors='replace') as f_out:
+        reorder = ireorder()
+        if reorder == "strict":
             strict(f_in, f_out)
-        elif rmethod == "blocks":
+        elif reorder == "blocks":
             blocks(f_in, f_out)
         else:
             print("THERE'S NO SUCH METHOD, TYPE IN ONE OF THE FOLLOWING.")
-            reorder()
+            execute()
 
 
-reorder()
+execute()
