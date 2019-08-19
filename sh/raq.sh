def95359b6d8ccfef2dd006a04cb5c47354b1683
WANDEX <15724752+WANDEX@users.noreply.github.com>
2019-08-19 12:58:32 +0300

    changed comment line removed unneeded android image

--- sh/raq.sh
+++ sh/raq.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# should run as root for hardware access for acceleration
+# must run with root privileges to access hardware acceleration
 qemu-system-x86_64 -m 4G -boot d -cpu host -smp 4 -machine type=q35,accel=kvm -net nic -net user,hostfwd=tcp::4444-:5555 -hda android.img -usb -device usb-tablet -display sdl -vga std -soundhw all -audiodev id=pa,driver=pa,server=unix:/run/user/1000/pulse/native,out.name=combined
-#qemu-system-x86_64 -m 4G -boot d -cpu host -smp 4 -machine type=q35,accel=kvm -net nic -net user,hostfwd=tcp::4444-:5555 -hda /var/lib/libvirt/images/android.qcow2 -usb -device usb-tablet
