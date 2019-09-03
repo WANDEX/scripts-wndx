0b0694d76b60daa6d2349a5b0017e054457aec7f
WANDEX <15724752+WANDEX@users.noreply.github.com>
2019-09-03 23:40:51 +0300

    modified to run commands strictly line by line

--- sh/arun.sh
+++ sh/arun.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-sleep 3
-sh arun_telegram.sh &>/dev/null &
-sleep 2
-sh arun_pulseeffects.sh &>/dev/null &
-sleep 2
-sh arun_ncmpcpp.sh &>/dev/null &
-sleep 3
-sh arun_chrome.sh &>/dev/null &
+sleep 3 ;
+sh arun_telegram.sh &>/dev/null ;
+sleep 2 ;
+sh arun_pulseeffects.sh &>/dev/null ;
+sleep 2 ;
+sh arun_ncmpcpp.sh &>/dev/null ;
+sleep 3 ;
+sh arun_chrome.sh &>/dev/null ;
