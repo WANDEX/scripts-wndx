47f27c21384cba2e9bd50b121c053bb00c2c3d84
WANDEX <15724752+WANDEX@users.noreply.github.com>
2019-10-12 01:00:19 +0300

    prints all files and their dir paths at provided dir

--- py/group_files_by_name.py
+++ py/group_files_by_name.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 import os
 from collections import Counter
+from itertools import count
 
 S_PATH = "/home/wndx/Downloads/Pictures/hotline_miami/images/"
 l_fullpath = []
+l_filenames = []
 
 
 def validate_path(s_path):
@@ -17,17 +19,24 @@ def validate_path(s_path):
 
 
 def get_file_paths(s_path):
+    d_fullpath = {}
     for (dirpath, dirnames, filenames) in os.walk(s_path):
-        l_fullpath.extend(os.path.join(dirpath, filename) for filename in filenames)
+        for filename in filenames:
+            d_fullpath.update({dirpath: filename})
+            l_fullpath.extend(d_fullpath.items())
 
-    # print("file_count:{0}".format(len(l_fullpath)))
+
+def line_by_line():
+    for line in l_fullpath:
+        print("{0}".format(line))
 
 
 def main():
     validate_path(S_PATH)
     get_file_paths(S_PATH)
-    print(l_fullpath)
-    print("files:{0}".format(len(l_fullpath)))
+    # print(l_fullpath)
+    line_by_line()
+    print(len(l_fullpath))
 
 
 main()
