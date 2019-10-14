d21a9020aa70c1526f02419b90fc06c650e6b948
WANDEX <15724752+WANDEX@users.noreply.github.com>
2019-10-14 19:42:18 +0300

    sorted dict etc

--- py/group_files_by_name.py
+++ py/group_files_by_name.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-from collections import Counter
+import collections
+from operator import itemgetter
 from itertools import count
 
 S_PATH = "/home/wndx/Downloads/Pictures/hotline_miami/images/"
 l_fullpath = []
-l_filenames = []
 
 
 def validate_path(s_path):
@@ -24,6 +24,7 @@ def get_file_paths(s_path):
         for filename in filenames:
             d_fullpath.update({dirpath: filename})
             l_fullpath.extend(d_fullpath.items())
+    l_fullpath.sort(key=itemgetter(1))
 
 
 def line_by_line():
@@ -31,12 +32,43 @@ def line_by_line():
         print("{0}".format(line))
 
 
+def long_substr(data):
+    substr = ""
+    if len(data) > 1 and len(data[0]) > 0:
+        for i in range(len(data[0])):
+            for j in range(len(data[0]) - i + 1):
+                if j > len(substr) and is_substr(data[0][i:i+j], data):
+                    substr = data[0][i:i+j]
+    return substr
+
+
+def is_substr(find, data):
+    if len(data) < 1 and len(find) < 1:
+        return False
+    for i in range(len(data)):
+        if find not in data[i]:
+            return False
+    return True
+
+
+def longest_common_substring():
+    l_filenames = []
+    for line in l_fullpath:
+        l_filenames.append(line[1])
+        # print("{0}".format(line[1]))
+    print(l_filenames[0:10])
+    long_substr(l_filenames)
+
+
 def main():
     validate_path(S_PATH)
     get_file_paths(S_PATH)
     # print(l_fullpath)
     line_by_line()
+    # print(long_substr())
+    # print(l_fullpath.pop(0)[1])
     print(len(l_fullpath))
+    # longest_common_substring()
 
 
 main()
