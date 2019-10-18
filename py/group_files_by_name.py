a9b5fab5d9af4738337ef8a202e7430bfb1e975f
WANDEX <15724752+WANDEX@users.noreply.github.com>
2019-10-18 15:47:29 +0300

    get_prefixes(f_list)

--- py/group_files_by_name.py
+++ py/group_files_by_name.py
@@ -61,6 +61,36 @@ def get_common_start(seq):
             return s1[0:i]
     return s1[0:length]
 
+
+def get_prefixes(f_list):
+    common_prefix = [""]
+    extensible_f_list = []
+    temp_f_list = []
+    last_element = itemgetter(-1)
+    for file in f_list:
+        extensible_f_list.append(file)
+        print("extensible_f_list:{0}".format(extensible_f_list))
+        print("file:{0}".format(file))
+
+        if len(get_common_start(extensible_f_list)) > 0:
+            temp_f_list = extensible_f_list.copy()
+        else:
+            print("clear at file:{0}".format(file))
+            extensible_f_list.clear()
+
+        if len(get_common_start(extensible_f_list)) == 0:
+            extensible_f_list.append(file)
+            if last_element(common_prefix) != get_common_start(temp_f_list):
+                common_prefix.append(get_common_start(temp_f_list))
+
+        print("temp_f_list:{0}".format(temp_f_list))
+        print("prefix:\t{0}".format(last_element(common_prefix)))
+        print()
+    if last_element(common_prefix) != get_common_start(temp_f_list):
+        common_prefix.append(get_common_start(temp_f_list))
+    common_prefix.pop(0)  # remove first empty element
+    print("common_prefix:{0}".format(common_prefix))
+
 def main():
     test_list = ["objMonitors_0.png", "objMonitors_1.png", "objMonitors_2.png", "shit"]
     validate_path(S_PATH)
