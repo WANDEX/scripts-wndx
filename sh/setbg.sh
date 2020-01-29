7f082ca90644c80aa98861ce3272a272f938f47f
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-01-29 23:33:51 +0300

    upd xwallpaper command inside setbg.sh

--- sh/setbg.sh
+++ sh/setbg.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 [ ! -z "$1" ] && cp "$1" ~/.config/wallpaper.png
-xwallpaper --zoom ~/.config/wallpaper.png
-#feh --bg-fill ~/.config/wallpaper.png --no-fehbg
+xwallpaper "$2" ~/.config/wallpaper.png
