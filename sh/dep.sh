da75f9355ebdef947fedac5876a66730862f3e76
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-04-07 19:03:21 +0300

    fixed and updated dep.sh

--- sh/dep.sh
+++ sh/dep.sh
@@ -3,36 +3,20 @@
 
 # colors example
 # printf "%s\n" "Text in ${red}red${end}, white and ${blu}blue${end}."
-red=$'\e[1;31m'
-grn=$'\e[1;32m'
-yel=$'\e[1;33m'
-blu=$'\e[1;34m'
-mag=$'\e[1;35m'
-cyn=$'\e[1;36m'
-end=$'\e[0m'
+red=$'\e[1;31m'; grn=$'\e[1;32m'; yel=$'\e[1;33m'; blu=$'\e[1;34m'; mag=$'\e[1;35m'; cyn=$'\e[1;36m'; end=$'\e[0m'
 
 # list libs required by provided binary
 LIBS=$(objdump -p $1 | grep NEEDED | awk '{ $1=""; print substr($0,2) }')
 
-exitCode() {
-    ### Exit codes:
-    ### 0 = match found
-    ### 1 = no match found
-    ### 2 = error
-    binaryF=$1
-    pacman -F $binaryF | grep -q installed | echo $?
-}
-
 while IFS= read -r line; do
-    statusMsg=""
-    statusCode="$(exitCode "$line")"
-    if [ "$statusCode" == "0" ]; then
-        statusMsg="${cyn}[installed]${end}..."
-    elif [ "$statusCode" == "1" ]; then
-        statusMsg="${red}[REQUIRED]${end}...."
-    else
-        statusMsg="${red}[ERROR]${end}......."
-    fi
+    ## Exit codes: 0 = match found; 1 = no match found; 2 = error;
+    pacman -F $line | grep -q installed
+    case "$?" in
+        0) statusMsg="${cyn}[installed]${end}...";;
+        1) statusMsg="${red}[REQUIRED]${end}....";;
+        2) statusMsg="${red}[ERROR]${end}.......";;
+        *) statusMsg="${red}[??? WTF ???]${end}.";;
+    esac >/dev/null
     printf "$statusMsg $line\n"
 done <<< "$LIBS"
 
