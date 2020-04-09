a1166e9432fbf612834bb4f2604ec292a214b548
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-04-09 23:16:28 +0300

    added support of optional array of flags

--- sh/download_audio.sh
+++ sh/download_audio.sh
@@ -10,19 +10,24 @@ OUT="${3:-"$MUSIC"}"
 # substring
 case "$URL" in
     *"bandcamp"*)
-        OUT+='~bandcamp/%(playlist)s/%(playlist_index)02d. %(title)s.%(ext)s'
+        OUT+='~bandcamp/%(artist)s/%(playlist)s/%(playlist_index)02d. %(title)s.%(ext)s'
+        OPT=( --embed-thumbnail )
     ;;
     *"soundcloud"*"/sets/"*)
         OUT+='~soundcloud/%(uploader)s/%(playlist)s/%(playlist_index)02d. %(fulltitle)s.%(ext)s'
+        OPT=( --embed-thumbnail )
     ;;
     *"soundcloud"*)
         OUT+='~soundcloud/%(uploader)s/%(playlist)s/%(fulltitle)s.%(ext)s'
+        OPT=( --embed-thumbnail )
     ;;
     *"youtu"*)
         OUT+='~youtube/%(playlist_title)s/%(playlist_index)02d. %(title)s.%(ext)s'
+        OPT=()
     ;;
     *)
         OUT+='~other/%(title)s.%(ext)s'
+        OPT=()
     ;;
 esac >/dev/null
 
@@ -32,7 +37,7 @@ FORMAT="$BEST"'/'"$FALLBACK"
 notify-send -t 3000 "Downloading..."
 time youtube-dl --ignore-errors --yes-playlist --playlist-end="$END" \
     --format "$FORMAT" --output "$OUT" --restrict-filenames \
-    --extract-audio --audio-format "mp3" "$URL" && \
+    --extract-audio --audio-format "mp3" "${OPT[@]}" "$URL" && \
     notify-send -u normal -t 8000 "COMPLETED" "Downloading and Converting." || \
     notify-send -u critical -t 5000 "ERROR" "Something gone wrong!"
 
