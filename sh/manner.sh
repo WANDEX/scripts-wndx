d3f760169d8019cac1345492d5e578cf33b42030
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-04-10 12:59:25 +0300

    added dmenu prompt

--- sh/manner.sh
+++ sh/manner.sh
@@ -2,11 +2,11 @@
 # search locally and open man page in $MANPAGER
 if [ -t 0 ]; then
     # execute in current $TERMINAL (already spawned)
-    man -k . | dmenu -l 30 | awk '{print $1}' | xargs -r man
+    man -k . | dmenu -p 'enter man page: ' -l 30 | awk '{print $1}' | xargs -r man
 else
     # spawn $TERMINAL and execute (works with dmenu)
-    man -k . | dmenu -l 30 | awk '{print $1}' | xargs -r $TERMINAL -e man
+    man -k . | dmenu -p 'enter man page: ' -l 30 | awk '{print $1}' | xargs -r $TERMINAL -e man
 fi
 
 ## open man page as pdf in zathura
-#man -k . | dmenu -l 30 | awk '{print $1}' | xargs -r man -Tpdf | zathura -
+#man -k . | dmenu -p 'enter man page: ' -l 30 | awk '{print $1}' | xargs -r man -Tpdf | zathura -
