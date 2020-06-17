0d0bed31a2c4c3c02a3398ca1398a9d3a621ba8d
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-06-17 18:49:44 +0300

    upd kb.sh

--- sh/kb.sh
+++ sh/kb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-setxkbmap -model pc104 -layout us,ru -variant colemak,  -option grp:caps_toggle,grp_led:scroll
+setxkbmap -model pc104 -layout us,ru -variant colemak,  -option grp:caps_toggle,grp_led:scroll &&
 xset r rate 200 50
