94343014833f54f5e553bdf7611a4a3f00e2fbbb
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-06-17 17:51:09 +0300

    completely redesigned script execution method from hard coded script names to automatically select them from the directory

--- sh/workspace/i3wm/arun/arun_i3wm.sh
+++ sh/workspace/i3wm/arun/arun_i3wm.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-sleep 3
-sh arun_telegram.sh &>/dev/null &
-sleep 2
-sh arun_pulseeffects.sh &>/dev/null &
-sleep 2
-sh arun_ncmpcpp.sh &>/dev/null &
-sleep 2
-sh arun_chrome.sh &>/dev/null &
-sleep 2
-i3-msg workspace $WS1
+# auto run (execute) scripts inside this file dir
+
+task() {
+    echo "$1" | xargs -t -o -I % sh -c 'sleep 0.5 && % &>/dev/null'
+}
+
+main() {
+    local abs_dir_path=$(dirname $(realpath "$0"))
+    # get abs path of all files excluding .dot files and this file itself
+    local scripts=$(find "$abs_dir_path" -type f -executable \( ! -iname ".*" ! -iname $(basename "$0") \))
+
+    for script in "$scripts"; do
+        task "$script"
+    done
+
+    sleep 2 && i3-msg -q workspace $WS1
+}
+
+main "$@"
+
