a218735cc71075f6181796254eb2ebeb7a26c317
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-06-17 15:22:51 +0300

    small tweak

--- sh/workspace/i3wm/arun/arun_ncmpcpp.sh
+++ sh/workspace/i3wm/arun/arun_ncmpcpp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 i3-msg workspace $WS10
-$TERMINAL -e ncmpcpp --config /home/wndx/.config/ncmpcpp/config
+$TERMINAL -e ncmpcpp &
--- sh/workspace/i3wm/arun/arun_pulseeffects.sh
+++ sh/workspace/i3wm/arun/arun_pulseeffects.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 i3-msg workspace $WS8
-pulseeffects
+pulseeffects &
