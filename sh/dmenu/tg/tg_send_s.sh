fdeb7179ec643216405eeefb573396f248ef5929
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-06-29 20:48:22 +0300

    added dmenu scripts

--- sh/dmenu/tg/tg_send_s.sh
+++ sh/dmenu/tg/tg_send_s.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
-# select video from dir and subdirs via dmenu
-# copy selected video path to clipboard and send it via tg
+# select screenshot from dir and subdirs via dmenu
+# copy selected screenshot path to clipboard and send it via tg
 # in pre-opened recipient dialog! tdesktop shortcut: Send File Ctrl + O
-DIR="$HOME"'/Films/.mpv_encode/tg/'
+DIR="$HOME"'/Downloads/Pictures/mpv_screen'
 
 # find all files at path and ignore hidden .dot files
 files="$(find "$DIR" -type f \( ! -iname ".*" \))"
-names="$(basename --multiple $files)"
-echo "$names" | dmenu -i -p '(TG) choose video to send: ' -l 30 | xclip -in || exit 0
+names="$(basename --multiple $files | sort -nr)"
+echo "$names" | dmenu -i -p '(TG) choose screenshot to send: ' -l 30 | xclip -in || exit 0
 selection="$(xclip -out)""*"
 full_path="$(find "$DIR" -type f \( -iname "$selection" \))"
 # add to clipboard
 echo "$full_path" | xclip -in -sel clip -r
 
 # get tg PID
-PID=$(xdotool search --class TelegramDesktop getwindowpid)
+PID=$(xdotool search --class "telegram-desktop" getwindowpid)
+# get tg window id
+#WID=$(xdotool search --class "telegram-desktop" | head -1)
+#xdotool windowactivate --sync "$WID"
+
+#sleep 1.5
+#wait
+
 # in pre-opened recipient dialog! - open file select window
+# broken right now
 xdotool search --all --pid $PID --name "Telegram" key --clearmodifiers ctrl+o
 # paste full path -> select file
 xdotool search --sync --onlyvisible --all --pid $PID --name "Choose files" key --clearmodifiers ctrl+v Return
