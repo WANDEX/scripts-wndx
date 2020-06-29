ef1fe5a2d506ca439c47480b4a570c1b7e2ec0a9
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-06-29 20:50:54 +0300

    modified xorg scripts

--- sh/xorg/nvidia.sh
+++ sh/xorg/nvidia.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-OUT0=$OUT0
-OUT1=$OUT1
-nvidia-settings --assign CurrentMetaMode="$OUT1: 1280x1024_60 +1920+0, $OUT0: 1920x1080_60 +0+0" &&
-xrandr --output $OUT0 --primary &&
-i3 restart
+nvidia-settings --assign CurrentMetaMode="$OUT0: 1920x1080_60 +0+0, $OUT1: 1280x1024_60 +1920+0" &&
+xrandr --output $OUT0 --primary
--- sh/xorg/xrandr.sh
+++ sh/xorg/xrandr.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # env OUTS declared in pam env /etc/environment
-OUT0=$OUT0
-OUT1=$OUT1
+# make sure nouveau not blacklisted, usually in /usr/lib/modprobe.d/nvidia.conf
+# https://wiki.archlinux.org/index.php/Nouveau
 xrandr --output $OUT0 --rate 60 --pos 0x0 --primary &&
 xrandr --output $OUT1 --rate 60 --pos 1920x0 &&
 xrandr --setmonitor BenQ\ EW2440L 1920/531x1080/298+0+0 $OUT0 &&
-xrandr --setmonitor Samsung\ SyncMaster 1280/375x1024/300+1920+0 $OUT1 &&
-i3 restart
+xrandr --setmonitor Samsung\ SyncMaster 1280/375x1024/300+1920+0 $OUT1
+
