03d4ca238790b9dccd9c0c518d06dbd850fc1333
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-08-11 18:52:16 +0300

    upd switch_pulse_preset and pulse_preset

--- sh/switch_pulse_preset.sh
+++ sh/switch_pulse_preset.sh
@@ -5,32 +5,32 @@ OUTPUT_ARRAY=($( ls "$OUTPUT_DIR" | cut -f1 -d"." | sort -h | tr '\n' ' ' ))
 OUTPUT_LENGTH=${#OUTPUT_ARRAY[@]}
 COLORS_ARRAY=(7 5 13)
 COLORS_LENGTH=${#COLORS_ARRAY[@]}
-FILE="$SCRIPTS"'/.temp/.switch_pulse_preset_status.sh'
-CURRENT_PRESET_NAME=$( "$FILE" | head -n 1 )
+FILE="$CSCRDIR"'/pulse_active_preset'
+CURRENT_PRESET_NAME=$(cat "$FILE" | head -n 1)
 X_RES_COLORS_COUNT=$(($(xrdb -query | grep '*.color' | awk '{print $2}' | wc -w) - 1))
 
 echo "${OUTPUT_ARRAY[@]}"
 for (( i=0; i<$OUTPUT_LENGTH; i++ )); do
     # if count of predefined colors < available presets, append new color N
-    if [[ "$COLORS_LENGTH" < "$OUTPUT_LENGTH" ]]; then
-        random_int=$(shuf -i 1-"$X_RES_COLORS_COUNT" -n 1)
-        COLORS_ARRAY+=("$random_int")
+    if [[ $COLORS_LENGTH < $OUTPUT_LENGTH ]]; then
+        random_int=$(shuf -i 1-$X_RES_COLORS_COUNT -n 1)
+        COLORS_ARRAY+=($random_int)
         COLORS_LENGTH=${#COLORS_ARRAY[@]}
     fi
     # find index of current preset out of all available presets
     if [[ "$CURRENT_PRESET_NAME" == "${OUTPUT_ARRAY[i]}" ]]; then
         # if there is no next element, get first
         if [[ -z "${OUTPUT_ARRAY[$i+1]}" ]]; then
-            echo "${OUTPUT_ARRAY[0]}"
             NEW_PRESET_NAME="${OUTPUT_ARRAY[0]}"
             COLOR_INT="${COLORS_ARRAY[0]}"
         else
-            echo "${OUTPUT_ARRAY[$i+1]}"
             NEW_PRESET_NAME="${OUTPUT_ARRAY[$i+1]}"
             COLOR_INT="${COLORS_ARRAY[$i+1]}"
         fi
     fi
 done
+
+echo "$NEW_PRESET_NAME"
 
 # get Xresource color by N
 X_RES_COLOR=$( xrdb -query | grep '*.color'"$COLOR_INT" | awk '{print $2}' )
@@ -40,4 +40,4 @@ pulseeffects -l "$NEW_PRESET_NAME"
 # change at pulseeffects GUI (only preset name)
 gsettings set com.github.wwmm.pulseeffects last-used-preset "$NEW_PRESET_NAME"
 # write to file
-printf "#!/bin/sh\necho '"$NEW_PRESET_NAME"'\necho '"$X_RES_COLOR"'" > "$FILE"
+echo -e "$NEW_PRESET_NAME\n$X_RES_COLOR" > "$FILE"
