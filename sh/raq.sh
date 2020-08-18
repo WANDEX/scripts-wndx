03b5556e7213e3427999557b13afe43b4b0e4b52
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-08-19 00:42:38 +0300

    upd raq - qemu-system-x86_64 new audio options for new qemu

--- sh/raq.sh
+++ sh/raq.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-# must run with root privileges to access hardware acceleration
-qemu-system-x86_64 -m 4G -boot d -cpu host -smp 4 -machine type=q35,accel=kvm -net nic -net user,hostfwd=tcp::4444-:5555 -hda android.img -usb -device usb-tablet -display sdl -vga std -soundhw all -audiodev id=pa,driver=pa,server=unix:/run/user/1000/pulse/native,out.name=combined
+# must be run with root privileges to access hardware acceleration
+qemu-system-x86_64 -m 4G -boot d -cpu host -smp 4 -machine type=q35,accel=kvm -net nic -net user,hostfwd=tcp::4444-:5555 \
+    -hda ~/Downloads/android.img -usb -device usb-tablet -display sdl -vga std \
+    -audiodev pa,id=hda,out.format=f32,out.frequency=48000,out.name=combined,server=/run/user/1000/pulse/native, \
+    -device intel-hda -device hda-output,audiodev=hda
