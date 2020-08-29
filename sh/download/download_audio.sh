8f8bb5171ab2e41c4cdad6d28512ec9d14bcf177
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-08-29 10:07:27 +0300

    [download_audio.sh] added option for --restrict-filenames

--- sh/download/download_audio.sh
+++ sh/download/download_audio.sh
@@ -7,21 +7,23 @@ MUSIC="$HOME"'/Music/'
 read -d '' USAGE <<- EOF
 Usage: $(basename $BASH_SOURCE) [OPTION...]
 OPTIONS
-    -e, --end   If url is playlist - how many items to download (by default all:-1)
-    -h, --help  Display help
-    -p, --path  Destination path where to download
-    -u, --url   URL to download
+    -e, --end       If url is playlist - how many items to download (by default all:-1)
+    -h, --help      Display help
+    -p, --path      Destination path where to download
+    -r, --restrict  Restrict filenames to only ASCII characters, and avoid "&" and spaces in filenames
+    -u, --url       URL to download
 EOF
 
 get_opt() {
     # Parse and read OPTIONS command-line options
-    SHORT=e:hp:u:
-    LONG=end:,help,path:,url:
+    SHORT=e:hp:ru:
+    LONG=end:,help,path:,restrict,url:
     OPTIONS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
     # PLACE FOR OPTION DEFAULTS
     URL="$(xclip -selection clipboard -out)"
     END=-1
     OUT="$MUSIC"
+    restr=()
     eval set -- "$OPTIONS"
     while true; do
         case "$1" in
@@ -46,6 +48,9 @@ get_opt() {
         -p|--path)
             shift
             OUT="$1"
+            ;;
+        -r|--restrict)
+            restr=( --restrict-filenames )
             ;;
         -u|--url)
             shift
@@ -91,7 +96,7 @@ FALLBACK='bestaudio/best'
 FORMAT="$BEST"'/'"$FALLBACK"
 notify-send -t 3000 "Downloading..."
 time youtube-dl --ignore-errors --yes-playlist --playlist-end="$END" \
-    --format "$FORMAT" --output "$OUT" --restrict-filenames \
+    --format "$FORMAT" --output "$OUT" "${restr[@]}" \
     --extract-audio --audio-format "mp3" "${OPT[@]}" "$URL" && \
     notify-send -u normal -t 8000 "COMPLETED" "Downloading and Converting." || \
     notify-send -u critical -t 5000 "ERROR" "Something gone wrong!"
