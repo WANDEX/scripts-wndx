499e6c713812635fcbcb68aa63b3108339048f65
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-09-07 11:06:46 +0300

    [en.sh] added $1 to highlight letter

--- sh/en.sh
+++ sh/en.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-echo "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
+# use $1 to highlight letter
+echo "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z" | grep --color=auto -i "$1"
