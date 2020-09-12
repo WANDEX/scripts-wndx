7d35517e17c8449d065886ee85b671aacefb57ca
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-09-12 21:54:07 +0300

    [download_video.sh] added getopt options instead of positional arguments, --restrict-filenames, and some code refactoring

--- sh/download/download_video.sh
+++ sh/download/download_video.sh
@@ -1,23 +1,77 @@
 #!/bin/sh
 # download via youtube-dl video playlist or single video
 
-DIR="$HOME"'/Films/'
-# default video height cap by the 2nd argument,
-# will be less if unavailable in youtube-dl or default
-H_CAP='1080'
-# youtube-dl --playlist-end > get first N items from playlist
-# by the 3rd argument or default N - '-1' to download full playlist
-P_CAP='1'
-# prefer certain extension over FALLBACK in youtube-dl
-EXT='webm'
+# read into variable using 'Here Document' code block
+read -d '' USAGE <<- EOF
+Usage: $(basename $BASH_SOURCE) [OPTION...]
+OPTIONS
+    -e, --end       If url is playlist - how many items to download (default:1)
+    -h, --help      Display help
+    -p, --path      Destination path where to download
+    -q, --quality   Quality of video/stream
+    -r, --restrict  Restrict filenames to only ASCII characters, and avoid "&" and spaces in filenames
+    -u, --url       URL of video/stream
+EOF
 
-get_defaults() {
-    # use $1 or default (clipboard)
-    URL="${1:-$(xclip -selection clipboard -out)}"
-    END="${2:-"$P_CAP"}"
-    OUT="${3:-"$DIR"}"
-    QLT="${4:-"$H_CAP"}"
+get_opt() {
+    # Parse and read OPTIONS command-line options
+    SHORT=e:hp:rq:u:
+    LONG=end:,help,path:,restrict,quality:,url:
+    OPTIONS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
+    # PLACE FOR OPTION DEFAULTS
+    OUT="$HOME"'/Films/'
+    END=1      # youtube-dl --playlist-end > get first N items from playlist
+    EXT='webm' # prefer certain extension over FALLBACK in youtube-dl
+    QLT='1080' # video height cap, will be less if unavailable in youtube-dl
+    URL="$(xclip -selection clipboard -out)"
+    restr=()
+    eval set -- "$OPTIONS"
+    while true; do
+        case "$1" in
+        -e|--end)
+            shift
+            case $1 in
+                -1) END=-1 ;; # get full playlist
+                0*)
+                    printf "($1)\n^ unsupported number! exit.\n"
+                    exit 1
+                    ;;
+                ''|*[!0-9]*)
+                    printf "($1)\n^ IS NOT A NUMBER OF INT! exit.\n"
+                    exit 1
+                    ;;
+                *) END=$1 ;;
+            esac
+            ;;
+        -h|--help)
+            echo "$USAGE"
+            exit 0
+            ;;
+        -p|--path)
+            shift
+            OUT="$1"
+            ;;
+        -r|--restrict)
+            restr=( --restrict-filenames )
+            ;;
+        -q|--quality)
+            shift
+            QLT="$1"
+            ;;
+        -u|--url)
+            shift
+            URL="$1"
+            ;;
+        --)
+            shift
+            break
+            ;;
+        esac
+        shift
+    done
 }
+
+get_opt "$@"
 
 ytdl_check() {
     # youtube-dl URL verification, verify only first item if many
@@ -47,7 +101,7 @@ ytdl() {
             QLT="1080"
         ;;
         *)
-            QLT="$H_CAP"
+            QLT="$QLT"
         ;;
     esac >/dev/null
     VIDEO='bestvideo[ext='"$EXT"'][height<=?'"$QLT"']'
@@ -57,15 +111,14 @@ ytdl() {
     FALLBACKAUDIO='bestaudio/best'
     FORMAT="$GLUED"'/'"$FALLBACKVIDEO"'+'"$FALLBACKAUDIO"
     OUT+='.yt/%(playlist_title)s/%(playlist_index)003d. %(title)s.%(ext)s'
-    time youtube-dl --ignore-errors --yes-playlist --playlist-end="$END" \
+    youtube-dl --ignore-errors --yes-playlist --playlist-end="$END" \
         --write-sub --sub-lang en,ru --sub-format "ass/srt/best" --embed-subs \
-        --format "$FORMAT" --output "$OUT" "$URL" && \
+        --format "$FORMAT" --output "$OUT" "${restr[@]}" "$URL" && \
         notify-send -u normal -t 8000 "COMPLETED:" "Downloading and Converting. [VIDEO]" || \
         notify-send -u critical -t 5000 "ERROR:" "Something gone wrong! [VIDEO]"
 }
 
 main() {
-    get_defaults "$@"
     ytdl_check "$URL"
     ytdl "$@"
 }
