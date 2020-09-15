6c865ab42c01f1f95aef9dd81939755b8e8e23ac
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-09-15 11:19:11 +0300

    [kb.sh] added switching between qwerty/colemak keyboard variant

--- sh/kb.sh
+++ sh/kb.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-setxkbmap -model pc104 -layout us,ru -variant colemak,  -option grp:caps_toggle,grp_led:scroll &&
+CURRENT_VARIANT=$(setxkbmap -query | grep -i 'variant:' | awk '{print $2}' | sed "s/[,].*$//")
+if [[ $CURRENT_VARIANT != colemak ]]; then
+    setxkbmap -model pc104 -layout us,ru -variant colemak, -option grp:caps_toggle,grp_led:scroll
+    notify-send -u low "⌨ COLEMAK"
+else
+    setxkbmap -model pc104 -layout us,ru -option grp:caps_toggle,grp_led:scroll
+    notify-send -u low "⌨ QWERTY"
+fi
 xset r rate 200 50
+
