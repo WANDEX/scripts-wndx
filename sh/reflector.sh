8c7069164c0df21cfb5b7c57306ce6375349404c
WANDEX <15724752+WANDEX@users.noreply.github.com>
2020-09-24 00:34:43 +0300

    [reflector.sh] added notify-send status messages

--- sh/reflector.sh
+++ sh/reflector.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # requires sudo for updating servers in '/etc/pacman.d/mirrorlist'
-sudo reflector -p https --age 24 --fastest 50 --latest 25 --sort rate --save /etc/pacman.d/mirrorlist; rm -f /etc/pacman.d/mirrorlist.pacnew
+
+sudo reflector -p https --age 24 --fastest 50 --latest 25 --sort rate \
+    --save /etc/pacman.d/mirrorlist && \
+    notify-send "🆙 $(basename $0)" "mirrorlist updated" || \
+    notify-send -u critical "🆙 $(basename $0)" "error"
+rm -f /etc/pacman.d/mirrorlist.pacnew && \
+    notify-send -u low "🆙 $(basename $0)" "rm -f is OK" || \
+    notify-send -u critical "🆙 $(basename $0)" "rm error"
