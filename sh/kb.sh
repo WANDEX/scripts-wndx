ce287660a66705f5b12762c95268b90d346e5536
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-03-04 23:52:11 +0300

    [kb.sh] sh style minor changes

--- sh/kb.sh
+++ sh/kb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CURRENT_VARIANT=$(setxkbmap -query | grep -i 'variant:' | awk '{print $2}' | sed "s/[,].*$//")
-if [[ $CURRENT_VARIANT != colemak ]]; then
+if [ "$CURRENT_VARIANT" != "colemak" ]; then
     setxkbmap -model pc104 -layout us,ru -variant colemak, -option grp:caps_toggle,grp_led:scroll
     notify-send -u low "⌨ COLEMAK"
 else
