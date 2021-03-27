41315e91ef6908d0e310e2b1236ad3a49e23f36e
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-03-27 17:34:35 +0300

    [it.sh] notify-send -> dunstify

--- sh/it.sh
+++ sh/it.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # toggle Internet access.
 # sudo commands - requires sudo password
+DST="string:x-dunst-stack-tag"
 if route -v | grep -q "default" ; then
     sudo route del default gw 192.168.1.1 &&
-    notify-send --urgency=critical "OFF - default gateway deleted."
+    dunstify -t 3000 -u critical -h "$DST:Internet_toggle" -h "$DST:hi" "OFF - default gateway deleted."
 else
     # requires sudo password
     sudo route add default gw 192.168.1.1 &&
-    notify-send --urgency=critical "ON - default gateway added."
+    dunstify -t 3000 -u critical -h "$DST:Internet_toggle" -h "$DST:hi" "ON - default gateway added."
 fi
