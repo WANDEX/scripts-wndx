6bd1b34fdbdfb4c870d19db44a0a49222eda9032
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-05-06 18:30:48 +0300

    [restart_pipewire] restart sound related programs and pipewire itself

--- sh/restart_pipewire.sh
+++ sh/restart_pipewire.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
+# restart sound related programs and pipewire itself
+
+pulseeffects --quit
+
+# restart pipewire
 systemctl --user restart pipewire.service
+
+# run pipewire defaults configuration
+pipewire.sh
+wait
+
+mpd --kill
+mpd
+pkill mpdas
+setsid -f mpdas -d
+pkill mpdup
+setsid -f mpdup
+setsid -f pulseeffects --gapplication-service
+# use ^C to exit out of junk outputted by pulseeffects to terminal
+# WARNINGS etc. dunno how to suppress that...
