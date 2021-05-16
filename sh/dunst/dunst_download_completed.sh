dd08fb54cc4c36a1e1681d643e5a65fd3192ef62
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-05-16 13:54:13 +0300

    [dunst_download_completed.sh] fix: summary string

--- sh/dunst/dunst_download_completed.sh
+++ sh/dunst/dunst_download_completed.sh
@@ -8,7 +8,7 @@ icon="$4"
 urgency="$5"
 
 fwid=$(echo "$summary" | grep -o "(.*)" | sed "s/[()]//g")
-summary=$(echo "$summary" | sed "s/\(.*\)//g") # remove fwid and () from summary
+summary=$(echo "$summary" | sed "s/(.*)//g") # remove (fwid) from summary
 dunstify -u "$urgency" -h "string:x-dunst-stack-tag:dp_$fwid" "$summary" "$body"
 # we set WM_ICON_NAME as it's infinite loop abort condition in dunst_download_started.sh
 xprop -id "$fwid" -set WM_ICON_NAME "DOWNLOAD_COMPLETED"
