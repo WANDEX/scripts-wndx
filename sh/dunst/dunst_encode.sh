8d1e7025ab8c7bf4df8d398cfa776fc83be9218e
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-05-25 19:01:59 +0300

    [dunst_encode.sh] notify when process with encoding is finished
    and add ACTION to copy $out_path to clipboard or open video in mpv

---
+++ sh/dunst/dunst_encode.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# show notification when process with encoding is finished (vanished)
+# and add ACTION to copy $out_path to clipboard or open video in mpv
+appname="$1"
+summary="$2"
+body="$3"
+icon="$4"
+urgency="$5"
+
+summary="$(echo "$summary" | sed "s/STARTED/FINISHED/")"
+out_path=$(echo "$body" | grep -o "(.*)" | sed "s/[()]//g")
+H="string:x-dunst-stack-tag:dp_$out_path"
+body=$(echo "$body" | sed "s/(.*)//g") # remove (out_path) from body
+
+# wait till ffmpeg process with $out_path in command line is finished (vanished)
+pwait -u "$USER" -f "ffmpeg .*$out_path"
+
+ACTION=$(dunstify -u "$urgency" -h "$H" -A "default,clip,path" -A "mpv,open" "$summary" "\n$body")
+
+case "$ACTION" in
+"default")
+    clipargs "$out_path"
+    ;;
+"mpv")
+    setsid -f mpv "$out_path"
+    ;;
+esac
