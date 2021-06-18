4192b1cdfc77769b3f7bb2d6e3ff58e8ecca74c6
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-06-18 20:19:05 +0300

    [tg_send_s.sh] simplification and fixes

--- sh/dmenu/tg/tg_send_s.sh
+++ sh/dmenu/tg/tg_send_s.sh
@@ -5,27 +5,23 @@
 DIR="$HOME"'/Downloads/Pictures/mpv_screen'
 
 # find all files at path and ignore hidden .dot files
-files="$(find "$DIR" -type f \( ! -iname ".*" \))"
-names="$(basename --multiple $files | sort -nr)"
-echo "$names" | dmenu -i -p '(TG) choose screenshot to send: ' -l 30 | xclip -in || exit 0
-selection="$(xclip -out)""*"
-full_path="$(find "$DIR" -type f \( -iname "$selection" \))"
+names="$(find "$DIR" -type f \( ! -iname ".*" \) -printf '%f\n' | sort -nr)"
+selection="$(echo "$names" | dmenu -i -p '(TG) choose screenshot to send:' -l 30)"
+[ -z "$selection" ] && exit 0
+full_path="$DIR/$selection"
+if [ ! -f "$full_path" ]; then
+    notify-send -u critical "[tg_send] ERROR: file not found:" "\n$full_path\n"
+    exit 1
+fi
+
 # add to clipboard
 echo "$full_path" | xclip -in -sel clip -r
 
 # get tg PID
 PID=$(xdotool search --class "telegram-desktop" getwindowpid)
-# get tg window id
-#WID=$(xdotool search --class "telegram-desktop" | head -1)
-#xdotool windowactivate --sync "$WID"
-
-#sleep 1.5
-#wait
-
 # in pre-opened recipient dialog! - open file select window
-# broken right now
-xdotool search --all --pid $PID --name "Telegram" key --clearmodifiers ctrl+o
+xdotool search --all --pid "$PID" --name "Telegram" key --clearmodifiers ctrl+o
 # paste full path -> select file
-xdotool search --sync --onlyvisible --all --pid $PID --name "Choose files" key --clearmodifiers ctrl+v Return
+xdotool search --sync --onlyvisible --all --pid "$PID" --name "Choose files" key --clearmodifiers ctrl+v Return
 xdotool keyup Return
 
