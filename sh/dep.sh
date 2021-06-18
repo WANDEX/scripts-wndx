cdf41552422f69d48177bd04b8572d37d35856d9
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-06-18 14:26:15 +0300

    [dep.sh] source & use globally defined terminal color variables

--- sh/dep.sh
+++ sh/dep.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 # check which dependency libs are not installed for given binary as first parameter.
 
-# colors example
-# printf "%s\n" "Text in ${red}red${end}, white and ${blu}blue${end}."
-red=$'\e[1;31m'; grn=$'\e[1;32m'; yel=$'\e[1;33m'; blu=$'\e[1;34m'; mag=$'\e[1;35m'; cyn=$'\e[1;36m'; end=$'\e[0m'
+# SOURCE GLOBALLY DEFINED TERMINAL COLOR VARIABLES
+# shellcheck disable=SC1091
+# shellcheck source=$ENVSCR/termcolors
+TC="$ENVSCR/termcolors" && [ -r "${TC}" ] && . "${TC}"
 
 # list libs required by provided binary
-LIBS=$(objdump -p $1 | grep NEEDED | awk '{ $1=""; print substr($0,2) }')
+LIBS=$(objdump -p "$1" | grep NEEDED | awk '{ $1=""; print substr($0,2) }')
 
 while IFS= read -r line; do
     ## Exit codes: 0 = match found; 1 = no match found; 2 = error;
-    pacman -F $line | grep -q installed
+    pacman -F "$line" | grep -q installed
     case "$?" in
-        0) statusMsg="${cyn}[installed]${end}...";;
-        1) statusMsg="${red}[REQUIRED]${end}....";;
-        2) statusMsg="${red}[ERROR]${end}.......";;
-        *) statusMsg="${red}[??? WTF ???]${end}.";;
+        0) statusMsg="${CYN}[installed]${END}...";;
+        1) statusMsg="${RED}[REQUIRED]${END}....";;
+        2) statusMsg="${RED}[ERROR]${END}.......";;
+        *) statusMsg="${RED}[??? WTF ???]${END}." && exit 1 ;;
     esac >/dev/null
-    printf "$statusMsg $line\n"
+    printf "%s %s\n" "$statusMsg" "$line"
 done <<< "$LIBS"
 
 printf "\nTo find out in which Repo & Package lib exists:\n"
-printf "${cyn}pacman -F${end} ...\n"
+printf "%s\n" "${CYN}pacman -F${END} ..."
