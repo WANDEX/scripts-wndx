9484ff5c62e00766f7390034101ccda90ff2796f
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-06-18 20:13:58 +0300

    [tg_send_v.sh] simplification and fixes

--- sh/dmenu/tg/tg_send_v.sh
+++ sh/dmenu/tg/tg_send_v.sh
@@ -2,22 +2,27 @@
 # select video from dir and subdirs via dmenu
 # copy selected video path to clipboard and send it via tg
 # in pre-opened recipient dialog! tdesktop shortcut: Send File Ctrl + O
-DIR="$HOME"'/Films/.mpv_encode/tg/'
+DIR="$HOME"'/Films/.mpv_encode/tg'
 
 # find all files at path and ignore hidden .dot files
-files="$(find "$DIR" -type f \( ! -iname ".*" \))"
-names="$(basename --multiple $files)"
-echo "$names" | dmenu -i -p '(TG) choose video to send: ' -l 30 | xclip -in || exit 0
-selection="$(xclip -out)""*"
-full_path="$(find "$DIR" -type f \( -iname "$selection" \))"
+names="$(find "$DIR" -type f \( ! -iname ".*" \) -printf '%f\n')"
+selection=$(echo "$names" | dmenu -i -p '(TG) choose video to send:' -l 30)
+[ -z "$selection" ] && exit 0
+# FIXME DOES NOT Escapes unusual characters on filenames!
+full_path="$(find "$DIR" -type f \( -name "$selection" \))"
+if [ ! -f "$full_path" ]; then
+    notify-send -u critical "[tg_send] ERROR: file not found:" "\n$full_path\n"
+    exit 1
+fi
+
 # add to clipboard
 echo "$full_path" | xclip -in -sel clip -r
 
 # get tg PID
 PID=$(xdotool search --class TelegramDesktop getwindowpid)
 # in pre-opened recipient dialog! - open file select window
-xdotool search --all --pid $PID --name "Telegram" key --clearmodifiers ctrl+o
+xdotool search --all --pid "$PID" --name "Telegram" key --clearmodifiers ctrl+o
 # paste full path -> select file
-xdotool search --sync --onlyvisible --all --pid $PID --name "Choose files" key --clearmodifiers ctrl+v Return
+xdotool search --sync --onlyvisible --all --pid "$PID" --name "Choose files" key --clearmodifiers ctrl+v Return
 xdotool keyup Return
 
