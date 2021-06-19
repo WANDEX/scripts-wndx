ce7f1f84691fab8a190ad8eb9d2917813449fa73
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-06-19 22:47:48 +0300

    [draw_text_ffmpeg.sh] fixes, -n, --name Name of the output video
    line with 'aevalsrc=0' add silent audio ->
    to be able to concat with other video files

--- sh/ffmpeg/draw_text_ffmpeg.sh
+++ sh/ffmpeg/draw_text_ffmpeg.sh
@@ -2,8 +2,8 @@
 # generate video file with text in the center of the screen
 
 # read into variable using 'Here Document' code block
-read -d '' USAGE <<- EOF
-Usage: $(basename $BASH_SOURCE) [OPTION...]
+read -r -d '' USAGE <<- EOF
+Usage: $(basename "$0") [OPTION...]
 OPTIONS
     -b, --background    Background color (default black)
     -d, --duration      Duration of the output video (default 1.0)
@@ -11,30 +11,30 @@ OPTIONS
     -g, --gte           Delay the display of text in seconds (default 0)
     -h, --help          Display help
     -i, --input         Use input video as background
+    -n, --name          Name of the output video
     -r, --resolution    Video resolution
     -s, --size          Font size
     -t, --text          Draw text on the screen
     --bordercolor       Font border color (default black)
     --borderwidth       Font border width (default 3)
     --fontcolor         Font foreground color (default white)
-EOF
-read -d '' EXAMPLE <<- "EOF"
-EXAMPLE
-    this_script -t 'Multiline \\
+EXAMPLES:
+    $(basename "$0") -t 'Multiline \\
     string \\
     example'
 EOF
 
 get_opt() {
     # Parse and read OPTIONS command-line options
-    SHORT=b:d:f:g:hi:r:s:t:
-    LONG=background:,duration:,font:,gte:,help,input:,resolution:,size:,text:,bordercolor:,borderwidth:,fontcolor:
+    SHORT=b:d:f:g:hi:n:r:s:t:
+    LONG=background:,duration:,font:,gte:,help,input:,name:,resolution:,size:,text:,bordercolor:,borderwidth:,fontcolor:
     OPTIONS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
     # PLACE FOR OPTION DEFAULTS
     background="black"
     duration=3.0
-    font_path="~/.local/share/fonts/roboto/Roboto_Slab/RobotoSlab-Light.ttf"
+    font_path="$HOME/.local/share/fonts/roboto/Roboto_Slab/RobotoSlab-Light.ttf"
     gte=0
+    name="output"
     resolution="1280x720"
     size=48
     text="Sample Text"
@@ -62,12 +62,15 @@ get_opt() {
             ;;
         -h|--help)
             echo "$USAGE"
-            echo "$EXAMPLE"
             exit 0
             ;;
         -i|--input)
             shift
             input="$1"
+            ;;
+        -n|--name)
+            shift
+            name="$1"
             ;;
         -r|--resolution)
             shift
@@ -104,24 +107,26 @@ get_opt() {
 
 get_opt "$@"
 common=(
-    "drawtext=fontfile="$font_path":\
+    "drawtext=fontfile=$font_path:\
     enable='gte(t,$gte)':\
     fontsize=$size:\
-    fontcolor="$fontcolor":\
-    bordercolor="$bordercolor":\
+    fontcolor=$fontcolor:\
+    bordercolor=$bordercolor:\
     borderw=$borderwidth:\
     x=(w-text_w)/2:\
     y=(h-text_h)/2:\
     text='$text'"\
-    output.mp4
+    "${name}.mp4"
 )
 
 #if input file specified
-if [ ! -z "$input" ]; then
-    ffmpeg -i "$input" -vf \
-    "${common[@]}"
+if [ -n "$input" ]; then
+    ffmpeg -y -i "$input" \
+    -vf "${common[@]}"
 else
-    ffmpeg -f lavfi -i color=c="$background":s="$resolution":d=$duration -vf \
-    "${common[@]}"
+    # line with 'aevalsrc=0' adds silent audio -> to be able to concat with other video files
+    ffmpeg -y -f lavfi -i color=c="$background":s="$resolution":d="$duration" \
+    -f lavfi -i aevalsrc=0 -map 0 -map 1 -shortest \
+    -vf "${common[@]}"
 fi
 
