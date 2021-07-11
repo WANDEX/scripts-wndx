a46436fdc3b69eed8b2fc3779b8156f422b82935
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-07-11 17:08:29 +0300

    [tcolors] print terminal color tests

---
+++ sh/test/tcolors.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# print terminal color tests
+
+if [ -z "$1" ]; then
+    colors_terminal_test_1.sh
+    colors_terminal_test_0.sh
+else
+    colors_terminal_test_2.sh
+    colors_terminal_test_3.sh
+fi
