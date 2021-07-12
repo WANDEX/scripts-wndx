c3b820f4d363efb44cfcd4d0364ad7b5d42a144e
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-07-12 23:59:24 +0300

    [dunst_web_scrobbler.sh] cleaning

--- sh/dunst/dunst_web_scrobbler.sh
+++ sh/dunst/dunst_web_scrobbler.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# do not show notification
 # get track information from notification, to display it in statusbar
+# do not show notification
 appname="$1"
 summary="$2"
 body="$3"
@@ -19,17 +19,9 @@ artist="$(echo "$body" \
     | sed "/^.* - Single/d; /^Your scrobbles:.*$/d; /$track/d; 4d" \
     | tr -d '\n')"
 
-# XXX delete me
-# dunstify -u "$urgency" -h "$DST:web-scrobbler" -h "$DST:hi" "$track - $artist"
-dunstify "$urgency" -u low -t 0 -h "$DST:web-scrobbler" -h "$DST:hi" "$track - $artist"
-
-# TODO maybe just write to file?
-
-# provide as arguments, first one 'web' is essential!
-# music web "$track $artist"
-# music-web "$track" "$artist"
+# dunstify "$urgency" -u low -t 0 -h "$DST:web-scrobbler" -h "$DST:hi" "$track - $artist"
 
 echo "$track - $artist" > "$FILE" # write to file in order to display via music-web script
 
 # hardcoded SIG! to update dwmblocks
-kill -45 "$(pidof dwmblocks)" # refresh music-web
+kill -46 "$(pidof dwmblocks)" # refresh music-web
