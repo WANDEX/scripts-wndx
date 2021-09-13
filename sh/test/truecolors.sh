d0777a05401deb46abde53d433bd06a010c62140
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-09-13 14:51:10 +0300

    [truecolors.sh] print smooth gradient transition of true colors

---
+++ sh/test/truecolors.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# print smooth gradient transition of true colors.
+# If everything goes well, you should be able to see a smooth color band.
+# If something is wrong, the color band will show obvious gradient between different parts.
+awk 'BEGIN{
+    s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
+    for (colnum = 0; colnum<77; colnum++) {
+        r = 255-(colnum*255/76);
+        g = (colnum*510/76);
+        b = (colnum*255/76);
+        if (g>255) g = 510-g;
+        printf "\033[48;2;%d;%d;%dm", r,g,b;
+        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+        printf "%s\033[0m", substr(s,colnum+1,1);
+    }
+    printf "\n";
+}'
