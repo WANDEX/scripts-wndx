aa96e6fc9e807d5c32ca7a00cd57ef5e0878bfc6
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-09-19 23:28:16 +0300

    [kb.sh] notify with dunstify if at path

--- sh/kb.sh
+++ sh/kb.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
+# toggle between keyboard layouts & show notification with current layout
+
+DST="string:x-dunst-stack-tag"
 CURRENT_VARIANT=$(setxkbmap -query | grep -i 'variant:' | awk '{print $2}' | sed "s/[,].*$//")
+
+at_path() { hash "$1" >/dev/null 2>&1 ;} # if $1 is found at $PATH -> return 0
+notify() {
+    if at_path dunstify; then
+        dunstify -u "low" -h "$DST:kb.sh" -h "$DST:hi" "$1"
+    else
+        notify-send -u low "$1"
+    fi
+}
+
 if [ "$CURRENT_VARIANT" != "colemak" ]; then
     setxkbmap -model pc104 -layout us,ru -variant colemak, -option grp:caps_toggle,grp_led:scroll
-    notify-send -u low "⌨ COLEMAK"
+    notify "⌨ COLEMAK"
 else
     setxkbmap -model pc104 -layout us,ru -option grp:caps_toggle,grp_led:scroll
-    notify-send -u low "⌨ QWERTY"
+    notify "⌨ QWERTY"
 fi
+
 xset r rate 200 50
 
