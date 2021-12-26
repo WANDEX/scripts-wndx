3f2c94a9a9aaadbb4194e0ba30884dc0b64c8ddd
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-12-26 22:36:37 +0300

    [venv.sh] auto find venv dir of the current project from any pwd

---
+++ sh/python/venv.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# auto find venv dir of the current project from any pwd
+# -> simply echo path to the activate script (to copy paste cmd)
+# simple echo because => cannot be sourced from non interactive shell
+git rev-parse --is-inside-work-tree  >/dev/null 2>&1 || exit 1 # suppress output & errors
+if [ -z "$VIRTUAL_ENV" ]; then
+    project_root="$(dirname "$(git rev-parse --git-dir)")"
+    activate_path="$(find "$project_root" -type f -path "*venv/bin/activate")"
+    if [ -n "$activate_path" ]; then
+        relative_path="$(realpath --no-symlinks --relative-to="$PWD" "$activate_path")"
+        echo ". $relative_path"
+    else
+        exit 3
+    fi
+else
+    echo "venv already active."
+    exit 2
+fi
