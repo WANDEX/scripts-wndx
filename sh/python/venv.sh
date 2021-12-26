7b935e1fe59d90de0c27b8fec6804505ed225957
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-12-26 22:50:00 +0300

    [venv.sh] if clipargs is found at $PATH -> also copy to clipboard

--- sh/python/venv.sh
+++ sh/python/venv.sh
@@ -8,7 +8,12 @@ if [ -z "$VIRTUAL_ENV" ]; then
     activate_path="$(find "$project_root" -type f -path "*venv/bin/activate")"
     if [ -n "$activate_path" ]; then
         relative_path="$(realpath --no-symlinks --relative-to="$PWD" "$activate_path")"
-        echo ". $relative_path"
+        cmd=". $relative_path"
+        if hash "clipargs" >/dev/null 2>&1; then
+            # if clipargs is found at $PATH -> also copy to clipboard
+            clipargs "$cmd"
+        fi
+        echo "$cmd"
     else
         exit 3
     fi
