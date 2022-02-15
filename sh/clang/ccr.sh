4aaa7796742fa2ceeaf76dccf603cfb35773652c
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-02-15 21:55:30 +0300

    [ccr.sh] basename replacing .ext on .out (to easy .gitignore *.out)

--- sh/clang/ccr.sh
+++ sh/clang/ccr.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-# clang compile into out file "basename" without extension & run it
+# clang compile into file "basename.out" & run it
 
-ext=""
+iext="" # in extension
+oext=".out" # out extension
 case "$1" in
     *.c)
-        ext=".c"
+        iext=".c"
     ;;
     *)
         printf "%s\n" "'$1' does not have supported .ext, exit."
         exit 3  # 3 - stands for Ext
     ;;
 esac
-# basename without .ext
-bname="$(basename "$1" "$ext")"
+[ -z "$iext" ] && exit 2
+# basename replacing .ext on .out
+bname="$(basename "$1" | sed "s/$iext$/$oext/")"
 clang -o "$bname" "$1" || exit 1
 "./$bname"
