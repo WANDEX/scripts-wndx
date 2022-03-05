9bd2777a1633e29fd24cec13f025be8773f563cb
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-03-05 11:45:07 +0300

    [qemu_oracle.sh] upd from ground 0

--- sh/qemu/qemu_oracle.sh
+++ sh/qemu/qemu_oracle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # qemu oracle linux system, specify additional options as arguments
-# for first installing specify: -cdrom iso_image -boot menu=on etc.
+# for first installing specify: -boot menu=on -cdrom iso_image etc.
 #
 # (to have access to all removable block devices -> define udev rule:
 # example: $ cat 55-removable-block-devices.rules
@@ -17,20 +17,17 @@
 # to mount usb-flash:
 # sudo mount -o defaults,uid=1000,gid=998 -v UUID=C0AB-9DEE "/mnt/usb_qemu" >/dev/null 2>&1
 
-# img="/mnt/main/DAUNLOD/ISO/oracle_linux/img/oracle_linux_8.4.cow"
-img="/mnt/main/DAUNLOD/ISO/oracle_linux/img/overlays/oracle_linux_8.4_nvim.cow"
+img="/mnt/main/DAUNLOD/ISO/oracle_linux/img/oracle_linux_8.4.cow"
+# img="/mnt/main/DAUNLOD/ISO/oracle_linux/img/overlays/oracle_linux_8.4_nvim.cow"
 
 # shellcheck disable=SC2068 # Double quote array expansions to avoid re-splitting elements.
 # -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:9001-:22 \
-# -vga cirrus \
-# -display sdl -vga std \
-# -machine q35 -cpu host -accel kvm \
 # -accel kvm \
 # -display sdl -vga std \
-# -cpu host -smp cores=2,threads=1 \
+# -cpu host -smp cpus=8,maxcpus=8,cores=4,threads=2 \
 qemu-system-x86_64 $@ -m 8G -usbdevice tablet \
 -machine type=q35,accel=kvm \
--cpu host -smp "$(nproc)" \
+-cpu host -smp cores=4,maxcpus=8 \
 -vga std \
 -device e1000,netdev=net0 -netdev user,id=net0 \
 "$img"
