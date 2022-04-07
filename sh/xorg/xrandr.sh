bacbb5009d6172098b17cc66aa606e1f852c3918
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-04-07 14:37:27 +0300

    [xorg] shellcheck fixes

--- sh/xorg/xrandr.sh
+++ sh/xorg/xrandr.sh
@@ -2,8 +2,8 @@
 # env OUTS declared in pam env /etc/environment
 # make sure nouveau not blacklisted, usually in /usr/lib/modprobe.d/nvidia.conf
 # https://wiki.archlinux.org/index.php/Nouveau
-xrandr --output $OUT0 --rate 60 --pos 0x0 --primary &&
-xrandr --output $OUT1 --rate 60 --pos 1920x0 &&
-xrandr --setmonitor BenQ\ EW2440L 1920/531x1080/298+0+0 $OUT0 &&
-xrandr --setmonitor Samsung\ SyncMaster 1280/375x1024/300+1920+0 $OUT1
+xrandr --output "$OUT0" --rate 60 --pos 0x0 --primary &&
+xrandr --output "$OUT1" --rate 60 --pos 1920x0 &&
+xrandr --setmonitor BenQ\ EW2440L 1920/531x1080/298+0+0 "$OUT0" &&
+xrandr --setmonitor Samsung\ SyncMaster 1280/375x1024/300+1920+0 "$OUT1"
 
