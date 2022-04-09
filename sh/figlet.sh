2c96e202cfe117aa50cdbd32661ed55f91ceefda
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-04-10 01:14:32 +0300

    [figlet.sh] huge general improvements, preview text in all fonts
    double quote text "$1" all other args for figlet options
    if only text given - preview given text in all found fonts

--- sh/figlet.sh
+++ sh/figlet.sh
@@ -1,9 +1,35 @@
-#!/bin/sh
-# don't forget to use double quotes "$1" "$2 ..." for multiple options
+#!/bin/bash
+# double quote text "$1" all other args for figlet options
+# if only text given - preview given text in all found fonts
+
+# set env var FIGLET_FONTDIR - path to search for font files
+FONTSDIR="${FIGLET_FONTDIR:-"/usr/share/figlet/fonts"}"
+
 TEXT="${1:-$(whoami)}"
-OPTIONS_ARRAY=($2)
-if [ -z "$2" ]; then
-    figlet -f lean -C upper "$TEXT" | tr ' _/' '/_'
+othr="${*/$1/}" # remove $1 from all other args
+IFS=" " read -r -a OPTS <<< "$othr" # array of figlet options
+
+at_path() { hash "$1" >/dev/null 2>&1 ;} # if $1 is found at $PATH -> return 0
+
+if [ -z "${OPTS[*]}" ]; then
+    [ -d "$FONTSDIR" ] || exit 1
+    if at_path fd; then
+        FONTS=$(fd . "$FONTSDIR" -tf -e flf -x basename "{/.}" | sort)
+    else
+        FONTS=$(find "$FONTSDIR" -type f -name '*.flf' -exec basename {} .flf ';' | sort)
+    fi
+    if [ -z "$FONTS" ]; then
+        echo "figlet fonts not found, exit."
+        exit 2
+    fi
+
+    for font in $FONTS; do
+        fout=$(figlet -f "$font" "$TEXT")
+        w=$(echo "$fout" | wc --max-line-length)
+        font_title=$(printf "%s%s%${w}s\n" "${CYN}${UND}" "$font" ">${END}")
+        out=$(printf "%s\n%s\n" "$font_title" "$fout")
+        echo "$out"
+    done
 else
-    figlet -f "${OPTIONS_ARRAY[@]}" "$TEXT"
+    figlet "${OPTS[@]}" "$TEXT"
 fi
