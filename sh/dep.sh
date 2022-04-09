43eb16d242c554aa9aeba160d0197213abe5175d
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-04-09 20:16:17 +0300

    [dep.sh] shellcheck fixes, general improvements

--- sh/dep.sh
+++ sh/dep.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 # check which dependency libs are not installed for given binary as first parameter.
+# $1 binary, $2 is optional: 'p' to just print libs
 
-# SOURCE GLOBALLY DEFINED TERMINAL COLOR VARIABLES
-# shellcheck disable=SC1091
-# shellcheck source=$ENVSCR/termcolors
-TC="$ENVSCR/termcolors" && [ -r "${TC}" ] && . "${TC}"
+if [ -z "$1" ]; then
+    echo "Provide binary as argument, exit."
+    exit 4
+fi
 
 # list libs required by provided binary
 LIBS=$(objdump -p "$1" | grep NEEDED | awk '{ $1=""; print substr($0,2) }')
+case "$2" in
+    p|s|print|show)
+        # just print libs of the binary
+        echo "$LIBS"
+        exit 0
+    ;;
+esac
 
-while IFS= read -r line; do
+for lib in $LIBS; do
     ## Exit codes: 0 = match found; 1 = no match found; 2 = error;
-    pacman -F "$line" | grep -q installed
+    pacman -F "$lib" | grep -q installed
     case "$?" in
         0) statusMsg="${CYN}[installed]${END}...";;
         1) statusMsg="${RED}[REQUIRED]${END}....";;
         2) statusMsg="${RED}[ERROR]${END}.......";;
         *) statusMsg="${RED}[??? WTF ???]${END}." && exit 1 ;;
     esac >/dev/null
-    printf "%s %s\n" "$statusMsg" "$line"
-done <<< "$LIBS"
+    printf "%s %s\n" "$statusMsg" "$lib"
+done
 
 printf "\nTo find out in which Repo & Package lib exists:\n"
 printf "%s\n" "${CYN}pacman -F${END} ..."
