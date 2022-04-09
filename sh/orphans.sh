f89dae1d91c4e87e0991907af2efeff14274b991
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-04-09 22:19:34 +0300

    [orphans.sh] shellcheck ignore rule

--- sh/orphans.sh
+++ sh/orphans.sh
@@ -2,4 +2,7 @@
 # remove real orphans (no longer needed packages)
 # also use sudo ncdu / or /var to see what to remove to free up space.
 # if deal with the pacman cache - sudo pacman -Sc (pacman -Scc to remove all cached files)
+
+# shellcheck disable=SC2046 #  Quote this to prevent word splitting
+# intentionally unquoted
 sudo pacman -Rns $(pacman -Qqtd)
