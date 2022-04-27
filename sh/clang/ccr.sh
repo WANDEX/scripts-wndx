6d183fd66eea4ffe88d1bd069c654f68b2183b93
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-04-27 12:14:35 +0300

    [ccr.sh] support .cpp extension, set default -std option, support any clang trailing args

--- sh/clang/ccr.sh
+++ sh/clang/ccr.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 # clang compile into file "basename.out" & run it
+# supports any clang options as trailing args after the file name
 
 iext="" # in extension
 oext=".out" # out extension
 case "$1" in
     *.c)
         iext=".c"
+        std="-std=gnu17"
+    ;;
+    *.cpp)
+        iext=".cpp"
+        std="-std=gnu++17"
+        pp="++"
     ;;
     *)
-        printf "%s\n" "'$1' does not have supported .ext, exit."
+        printf "\$1=%s\n" "'$1' does not have supported .ext, exit."
         exit 3  # 3 - stands for Ext
     ;;
 esac
 [ -z "$iext" ] && exit 2
-# basename replacing .ext on .out
-bname="$(basename "$1" | sed "s/$iext$/$oext/")"
-clang -o "$bname" "$1" || exit 1
-"./$bname"
+# out basename replacing .ext on .out
+oname=$(basename "$1" | sed "s/$iext$/$oext/")
+"clang${pp}" -Wall "$std" -o "$oname" "$@"  || exit 1
+"./$oname"
