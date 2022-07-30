b436114208659db6ed30eb0cadc1d4ae70d8a1db
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-07-30 12:02:04 +0300

    [release_pypi.sh] any twine options, support .pypirc data for upload

--- sh/python/release_pypi.sh
+++ sh/python/release_pypi.sh
@@ -4,6 +4,37 @@
 # exit if current dir does not have setup.py file
 [ -f ./setup.py ] || exit 1
 
+if [ -n "$PYPIRC" ]; then
+    pypirc_path="$PYPIRC"
+else
+    pypirc_path="$HOME/.pypirc"
+fi
+
+input_validation() {
+    # exit if $1 arg is not a simple argument (should be repository name in .pypirc)
+    err_txt="First arg should be arg for option --repository \$1 , exit."
+    case "$1" in
+        ''|--|-) # empty option
+            printf "%s\n" "$err_txt"
+            exit 2
+            ;;
+        --*|-*)
+            printf "%s\n" "$err_txt"
+            exit 3
+            ;;
+    esac
+}
+
+cdirname=$(basename "$(realpath ./)")
+
+if grep -qs "\[$cdirname\]" "$pypirc_path"; then
+    # .pypirc has repository entry with the exact current dir name (project name)
+    pypi_repo="$cdirname"
+else
+    input_validation "$1" # --repository $1
+    pypi_repo="" # NOTE: no need for assignment -> already the first arg of $@
+fi
+
 # get egg dir name, -d doesn't work with globs * (SC2144)
 egg_dir="$(find . -type d -path "./*.egg-info" | head -n1)"
 
@@ -13,8 +44,9 @@ egg_dir="$(find . -type d -path "./*.egg-info" | head -n1)"
 [ -d ./build/ ] && rm -rf ./build/
 printf "%s\n\n" "clean" && sleep 2
 
+# shellcheck disable=SC2068 # Double quote array expansions to avoid re-splitting elements
 # build => upload
 python setup.py sdist bdist_wheel &&
 printf "\n%s\n\n" "BUILT" && sleep 2 &&
-twine upload dist/* &&
+twine upload --repository "$pypi_repo" $@ --config-file "$pypirc_path" dist/* &&
 echo "DONE"
