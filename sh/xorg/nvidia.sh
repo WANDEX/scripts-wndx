10d5f130df1258aeb247baca9f49d03fdb8277ee
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-08-06 23:15:16 +0300

    [nvidia.sh] upd

--- sh/xorg/nvidia.sh
+++ sh/xorg/nvidia.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nvidia-settings --assign CurrentMetaMode="$OUT0: 1920x1080_60 +0+0, $OUT1: 1280x1024_60 +1920+0" &&
+nvidia-settings --assign CurrentMetaMode="$OUT0: 1920x1080_60 +0+0, $OUT1: 1920x1080_60 +1920+0" &&
 xrandr --output "$OUT0" --primary
