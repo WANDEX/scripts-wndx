da602e1fcbd0b4de3a6048c02ec9668b1a1bfb03
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-08-06 23:09:36 +0300

    [mouse_move.sh] minor: no need for extra ""

--- sh/fix/mouse_move.sh
+++ sh/fix/mouse_move.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # move mouse to the lower right corner of active window
 
-awin="$(xdotool getactivewindow)"
+awin=$(xdotool getactivewindow)
 # sed get first found match and print without match pattern
-geometry="$(xdotool getwindowgeometry "$awin" | sed -n "0,/  Geometry: /s///p")"
-gx="$(echo "$geometry" | cut -dx -f1)"
-gy="$(echo "$geometry" | cut -dx -f2)"
-new_x="$((gx-24))"
-new_y="$((gy-24))"
+geometry=$(xdotool getwindowgeometry "$awin" | sed -n "0,/  Geometry: /s///p")
+gx=$(echo "$geometry" | cut -dx -f1)
+gy=$(echo "$geometry" | cut -dx -f2)
+new_x=$((gx-24))
+new_y=$((gy-24))
 xdotool mousemove --window "$awin" "$new_x" "$new_y"
