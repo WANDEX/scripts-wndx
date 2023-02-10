a5290413454334cc3f781ee45225fd0f5c996181
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-02-10 20:29:53 +0300

    [dunst_twitch_ext.sh] fixes & improvements

--- sh/dunst/dunst_twitch_ext.sh
+++ sh/dunst/dunst_twitch_ext.sh
@@ -27,14 +27,22 @@
 # var mulstr = array.join("\n") // each on its own line
 # mulstr // to copy/paste into the file
 ## ``` // JS END
-##
-##
+
+set -e
+bname=$(basename "$0")
+
 # shellcheck disable=SC2034 # appears unused
+{ # {} to apply these ^ rules to the nested:
 appname="$1"
 summary="$2"
 body="$3"
 icon="$4"
 urgency="$5"
+}
+
+# toggle behavior of showing notifications (ALL/ONLY LISTED)
+# 1 - show all, otherwise suppress notifications of the not listed channels.
+TLC_SHOW_ALL="${TLC_SHOW_ALL:-0}"
 
 ## HACK: Extension sends channel name, not channel login.
 ##       Channel login is the actual thing used in construction of the channel $url.
@@ -44,33 +52,41 @@ urgency="$5"
 ## original notification format at the time of writing the script:
 ## s: 'Twitch Live Channels'
 ## b: '<b>LIRIK</b> is Live streaming Just Chatting'
-## XXX uncomment for debug (if you suspect that notification format has been changed)
-# printf "s: '%s'\nb: '%s'\n\n" "$summary" "$body" >> ~/tmp_tls_log.txt
 
-# toggle behavior of showing notifications (ALL/ONLY LISTED)
-# 1 - show all, otherwise suppress notifications of not listed channels
-TLC_SHOW_ALL=0
+dump_vars() {
+    printf "s: '%s'\nb: '%s'\n\n" "$summary" "$body" >> ~/dump_tlc_log.txt
+}
+
+## uncomment for debug (if you suspect that notification format has been changed)
+# dump_vars
 
 # sed get only channel name inside surrounding: <b> </b>
 twitch_channel=$(echo "$body" | sed -n "s/^.*>\(.*\)<.*$/\1/p")
-# remove all unprintable including spaces (just in case)
+# remove all unprintable characters & spaces (just in case)
 twitch_channel=$(echo "$twitch_channel" | tr -cd "[:print:]")
+
+# handle case when $twitch_channel is empty
+if [ -z "$twitch_channel" ]; then
+    dunstify -u critical "[$bname] ERROR: empty channel name" "\nvars dumped into the log file." &
+    dump_vars
+    exit 4
+fi
 
 category=$(echo "$body" | sed "s/^.*is Live streaming //")
 
 
 url="https://www.twitch.tv/${twitch_channel}"
 summary="🔴 $twitch_channel LIVE:"
-body="\n[TLC]:  $category\n$url"
+body="\n[TLC]: $category\n$url"
 
 # automatically open stream if channel if found in the auto_file (each on its own line)
 auto_file="$CSCRDIR/dunst_twitch"
 [ ! -f "$auto_file" ] && touch "$auto_file"
-if grep -i "$twitch_channel" "$auto_file"; then
+if grep -iqx "$twitch_channel" "$auto_file"; then
     DST="string:x-dunst-stack-tag"
     bg="string:bgcolor:#9147FF"
     fg="string:fgcolor:#EFEFF1"
-    dunstify -h "$DST:hi" -h "$bg" -h "$fg" "(AUTO) mpvu:" "$twitch_channel\n" &
+    dunstify -h "$DST:hi" -h "$bg" -h "$fg" "[$bname] (AUTO) mpvu:" "$twitch_channel\n" &
     setsid -f mpvu -u "$url"
 fi
 
@@ -79,7 +95,7 @@ if [ -n "$TLC_SHOW_ALL" ] && [ "$TLC_SHOW_ALL" != 1 ]; then
     # do not show notification if channel is not found in the notify_file (each on its own line)
     notify_file="$CSCRDIR/dunst_twitch_notify_list"
     [ ! -f "$notify_file" ] && touch "$notify_file"
-    if ! grep -i "$twitch_channel" "$notify_file"; then
+    if ! grep -iqx "$twitch_channel" "$notify_file"; then
         # do not show notification, this channel is not that important.
         # => channel (channel_login) is not found in notify_file, just silently exit.
         exit 0
