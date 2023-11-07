8cf3d2223deb8f229ac869ebb1d82399fb7ea950
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-11-07 13:47:54 +0300

    [xrandr_work.sh] added

---
+++ sh/xorg/xrandr_work.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# env OUTS declared in pam env /etc/environment
+# make sure nouveau not blacklisted, usually in /usr/lib/modprobe.d/nvidia.conf
+# https://wiki.archlinux.org/index.php/Nouveau
+OUT0="HDMI1"
+OUT1="HDMI2"
+xrandr --output "$OUT1" --rate 60 --pos 1920x0 --primary
+xrandr --output "$OUT0" --rate 60 --pos 0x0
+
