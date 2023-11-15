163b3569e917c35d696c27fd03d4c82ec1a881f1
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-11-15 17:25:23 +0300

    [foreach.sh] usage upd

--- sh/tool/foreach.sh
+++ sh/tool/foreach.sh
@@ -2,7 +2,7 @@
 # take input lines from pipe and for each line execute all provided arguments
 # provide -h flag to see examples.
 
-self="$(basename "$0")"
+bn=$(basename "$0")
 f="${BLD}${ITQ}"
 
 OLDIFS="$IFS"
@@ -10,27 +10,31 @@ OLDIFS="$IFS"
 NLIFS='
 '
 
-USAGE=$(printf "%s" "\
-Usage: 'any multiline data' | ${self} [ARGS...]
+USAGE="\
+Usage: 'pipe any multiline data' | ${bn} | [ARGS...]
+
 USAGE ${BLD}EXAMPLES:${END}
 
 ${f}# grep all files with pattern and sed -i replace pattern strings:${END}
-grep -l -r '\$BASH_SOURCE' | foreach.sh | xargs sed -i 's/\$BASH_SOURCE/\"\$0\"/'
+grep -l -r '\$BASH_SOURCE' | ${bn} | xargs sed -i 's/\$BASH_SOURCE/\"\$0\"/'
 ${f}# ^ same but with ripgrep:${END}
 rg -l
 
 ${f}# grep from each *.diff file in current dir:${END}
-find . -type f -name *.diff | foreach.sh | xargs grep -i 'Only in'
+find . -type f -name *.diff | ${bn} | xargs grep -i 'Only in'
 
 ${f}# sed -i delete lines with pattern strings in all *.diff files at current dir:${END}
-find . -type f -name *.diff | foreach.sh | xargs sed -i '/Only in/d'
+find . -type f -name *.diff | ${bn} | xargs sed -i '/Only in/d'
 
 ${f}# sed -i replace pattern strings in all *.diff files at current dir:${END}
-find . -type f -name *.diff | foreach.sh | xargs sed -i 's/Only in/SED_REPLACEMENT/g'
+find . -type f -name *.diff | ${bn} | xargs sed -i 's/Only in/SED_REPLACEMENT/g'
 
 ${f}# find all files in current dir except *bad* and *out*, sort, concatenate and redirect into file:${END}
-find . -type f -name *.diff \( ! -iname "*bad*" ! -iname "*out*" \) | sort -n | foreach.sh | xargs cat > bad.diff
-")
+find . -type f -name *.diff \( ! -iname *bad* ! -iname *out* \) | sort -n | ${bn} | xargs cat > bad.diff
+
+${f}# append text with the extra new line to the end of the files:${END}
+fd -t f . | ${bn} | xargs sed -i '$ a END_OF_THE_FILE\\\\n'
+"
 
 case "$1" in
     h|-h|--help|usage)
@@ -52,3 +56,4 @@ for line in $pipe; do
     echo "$line" "$@"
 done
 IFS="$OLDIFS" # restore
+
