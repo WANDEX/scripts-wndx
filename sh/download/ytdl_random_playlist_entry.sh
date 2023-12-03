38c06a883a9a3b2fdc162f86e7d0ef696bed76da
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-12-03 14:11:41 +0300

    [ytdl_random_playlist_entry.sh] overhaul & fixes

--- sh/download/ytdl_random_playlist_entry.sh
+++ sh/download/ytdl_random_playlist_entry.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
-# select one random element from playlist, get json info and open url in mpv
+# select one random element from playlist
+# get json data and open url in mpv as audio
 
-URL="${1:-$(xclip -selection clipboard -out)}"
-case "$URL" in
-    *"youtu"*"playlist"*) INFO=$(youtube-dl -j --flat-playlist "$URL" | shuf -n 1) ;;
+# change main executable: youtube-dl, yt-dlp
+ytdl_exec="yt-dlp"
+
+url="${1:-$(xclip -selection clipboard -out)}"
+case "$url" in
+    *"youtu"*"playlist"*|*"youtu"*"videos"*)
+        json=$($ytdl_exec -j --flat-playlist "$url" | shuf -n 1 | tr -d '[:cntrl:]')
+        ;;
     *) notify-send "[ERROR]: given URL is not youtube playlist" "\n$url" && exit 1 ;;
-esac >/dev/null
+esac
 
-title=$(echo "$INFO" | jq -r '.title')
-watch=$(echo "$INFO" | jq -r '.url')
-seconds=$(echo "$INFO" | jq -r '.duration')
-duration=$(date -d@"$seconds" -u +%H:%M:%S)
-url='https://www.youtube.com/watch?v='"$watch"
-notify-send "$title" "\n[$duration] $url"
-mpvu -q audio --quiet --end 1 --url "$url"
+id=$(     printf "%s" "$json" | jq -r '.id')
+url=$(    printf "%s" "$json" | jq -r '.url')
+title=$(  printf "%s" "$json" | jq -r '.title')
+seconds=$(printf "%s" "$json" | jq -r '.duration')
+duration=$(date -d"@${seconds}" -u +%H:%M:%S)
+
+res_url=""
+[  "$id" != "null" ] && res_url="https://www.youtube.com/watch?v=${id}"
+[ "$url" != "null" ] && res_url="$url"
+if [ -z "$res_url" ]; then
+    notify-send -u critical -t 0 "$title" \
+    "\n[$duration] .id || .url were not found in the json data" &
+    exit 4
+fi
+
+notify-send "$title" "\n[$duration] $res_url" &
+exec mpvu -q audio --quiet --end 1 --url "$res_url"
+
