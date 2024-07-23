133bdc1dd1026a2011075ad1d4916bd27862218d
WANDEX <15724752+WANDEX@users.noreply.github.com>
2024-07-23 15:43:37 +0300

    [xrandr_work.sh] upd

--- sh/xorg/xrandr_work.sh
+++ sh/xorg/xrandr_work.sh
@@ -4,6 +4,6 @@
 # https://wiki.archlinux.org/index.php/Nouveau
 OUT0="DP1"
 OUT1="HDMI2"
-xrandr --output "$OUT1" --rate 60 --pos 1920x0 --primary
-xrandr --output "$OUT0" --rate 60 --pos 0x0
+xrandr --output "$OUT1" --rate 60 --pos 0x0
+xrandr --output "$OUT0" --rate 60 --pos 1920x0 --primary
 
